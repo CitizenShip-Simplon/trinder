f9c442e2d67d804d297578db2866ce7a499186ec
Alex <revolalex@gmail.com>
2019-12-03 16:37:04 +0100

    Initial Commit

---
+++ Trinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trinder
+//
+//  Created by alexandre rodriguez on 03/12/2019.
+//  Copyright © 2019 alexandre rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
