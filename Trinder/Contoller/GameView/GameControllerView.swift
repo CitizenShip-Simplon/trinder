1a8915cca3a64a8584ac9ca1a408b5433edb3ae7
Alex <revolalex@gmail.com>
2019-12-13 10:06:00 +0100

    "work on game view"

---
+++ Trinder/Contoller/GameView/GameControllerView.swift
@@ -0,0 +1,30 @@
+//
+//  GameControllerView.swift
+//  Trinder
+//
+//  Created by alexandre rodriguez on 13/12/2019.
+//  Copyright © 2019 alexandre rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class GameControllerView: UIViewController{
+
+    @IBOutlet weak var rightLabel: UILabel!
+    @IBOutlet weak var leftLabel: UILabel!
+    @IBOutlet weak var dechetImageView: UIImageView!
+    
+    @IBOutlet weak var leftButton: UIButton!
+    
+    @IBOutlet weak var rightButton: UIButton!
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    }
+    
+
+    
+    
+    
+}
--- Trinder/Contoller/RightAnswerControllerView.swift
+++ Trinder/Contoller/RightAnswerControllerView.swift
@@ -17,8 +17,8 @@ class RightAnswerViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.r on fait un rule
     
-        let phrase = ["6 briques de lait -> 1 rouleau de papier toilette", "9 bouteilles en plastique transparent -> 1 tee-shirt", "8 boîtes de conserves -> casserole", "250 canettes -> 1 cadre de vélo", "1OOO Kg de verre -> 2 138 bouteilles de 75 cl"]
+        let phrase = ["6 briques de lait -> 1 rouleau de papier toilette", "9 bouteilles en plastique transparent -> 1 tee-shirt", "8 boîtes de conserves -> casserole", "250 canettes -> 1 cadre de vélo", "1OOO Kg de verre -> 2 138 bouteilles de 75 cl", "12 bouteilles plastique -> 1 oreiller", "1 boite de céréale -> 1 boite d'oeuf", "1 tonne de plastique recycler -> 800 Kg de pétrole économisé"]
         
-        labelRandom.text = phrase[Int.random(in: 0...4)]
+        labelRandom.text = phrase[Int.random(in: 0...7)]
     }
 }
