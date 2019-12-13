8d36bfbe0609f79335fad1602bc19531bc9b23bd
Haicheng WANG <haicheng.wang2@gmail.com>
2019-12-13 09:56:35 +0100

    petit maj

---
+++ Trinder/View/GameView/imageView.swift
@@ -0,0 +1,35 @@
+//
+//  imageView.swift
+//  Trinder
+//
+//  Created by Haicheng WANG on 12/12/2019.
+//  Copyright © 2019 alexandre rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class imageView: UIImageView {
+    
+//    @IBOutlet weak var image: UIImageView!
+//    
+//    let dicoImage = ["pomme": "pbVert", "carton": "pbJaune", "bouteille": "pbWhite"]
+//    var hasard = Int.random(in: 0...2)
+//    
+//    print(dicoImage[hasard])
+//    
+//    dicoImage[hasard].value + pb.random
+//    var hasarde = Int.random(in: 0...2)
+//    
+//    @IBAction func recy(_ sender: Any) {
+//    }
+//    @IBAction func reci(_ sender: Any) {
+//    }
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
