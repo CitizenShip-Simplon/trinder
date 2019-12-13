f6b0e727b2202f22c32c837989b0aa26fd4d838a
William <williamsouef@gmail.com>
2019-12-13 10:02:41 +0100

    "petit push du matin"

--- Trinder/View/GameView/WillViewController.swift
+++ Trinder/View/GameView/WillViewController.swift
@@ -14,6 +14,7 @@ class WillViewController: UIViewController {
     
     
     // var image passed = "" for image passed"1" in masque de Plongée print =
+//    aider pour le premier écran 
     
     struct Déchets {
            
